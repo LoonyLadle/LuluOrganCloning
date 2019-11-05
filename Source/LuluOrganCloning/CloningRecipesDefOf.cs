afe7a9fa005e3a59542cfde06a04d16290db4665
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-11-05 11:37:13 -0500

    Tabs everywhere!

--- Source/LuluOrganCloning/CloningRecipesDefOf.cs
+++ Source/LuluOrganCloning/CloningRecipesDefOf.cs
@@ -5,13 +5,13 @@ using Verse;
 
 namespace LoonyLadle.OrganCloning
 {
-   [DefOf]
-   public static class CloningRecipesDefOf
-   {
+	[DefOf]
+	public static class CloningRecipesDefOf
+	{
 		static CloningRecipesDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(CloningRecipesDefOf));
 
 		public static HediffDef LuluOrganCloning_ClonedOrgan;
-      public static RecipeDef LuluOrganCloning_PrepareOrgan;
-      public static RecipeDef LuluOrganCloning_HarvestOrgan;
-   }
+		public static RecipeDef LuluOrganCloning_PrepareOrgan;
+		public static RecipeDef LuluOrganCloning_HarvestOrgan;
+	}
 }
