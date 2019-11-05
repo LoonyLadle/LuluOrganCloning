a6fb328628dd22d930ba06b1c7d69d31ab91e15b
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-11-05 14:44:00 -0500

    Added EnsureInitializedInCtor

--- Source/LuluOrganCloning/CloningRecipesDefOf.cs
+++ Source/LuluOrganCloning/CloningRecipesDefOf.cs
@@ -8,6 +8,8 @@ namespace LoonyLadle.OrganCloning
 	[DefOf]
 	public static class CloningRecipesDefOf
 	{
+		static CloningRecipesDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(CloningRecipesDefOf));
+
 		public static HediffDef LuluOrganCloning_ClonedOrgan;
 		public static RecipeDef LuluOrganCloning_PrepareOrgan;
 		public static RecipeDef LuluOrganCloning_HarvestOrgan;
