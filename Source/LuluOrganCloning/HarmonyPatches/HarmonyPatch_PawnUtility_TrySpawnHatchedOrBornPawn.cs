cea37b0b2a7b53a3b3f49e8fecf3a8ee7332d769
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-11-05 14:47:30 -0500

    Split harmony patches into seperate classes

---
+++ Source/LuluOrganCloning/HarmonyPatches/HarmonyPatch_PawnUtility_TrySpawnHatchedOrBornPawn.cs
@@ -0,0 +1,41 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.OrganCloning
+{
+	[HarmonyPatch(typeof(PawnUtility), nameof(PawnUtility.TrySpawnHatchedOrBornPawn))]
+	public static class HarmonyPatch_PawnUtility_TrySpawnHatchedOrBornPawn
+	{
+		public static void Postfix(Pawn pawn, Thing motherOrEgg, bool __result)
+		{
+			try
+			{
+				// Only bother if the pawn wqas spawned, the parent is a pawn, and we can harvest cloned organs from this pawn.
+				if (__result && motherOrEgg is Pawn mother && CloningRecipesDefOf.LuluOrganCloning_HarvestOrgan.recipeUsers.Contains(mother.def))
+				{
+					// Get our pregnancy's organProps.
+					HediffComp_OrganProps organProps = mother.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Pregnant)?.TryGetComp<HediffComp_OrganProps>();
+
+					// This can happen if TrySpawnHatchedOrBornPawn is called from outside the pregnancy hediff, which some mods do.
+					if (organProps == null) return;
+
+					// Apply ther cloned organ hediff to every part specified in our organProps.
+					foreach (BodyPartRecord part in organProps.organsToClone)
+					{
+						pawn.health.AddHediff(HediffMaker.MakeHediff(CloningRecipesDefOf.LuluOrganCloning_ClonedOrgan, pawn, part));
+					}
+				}
+			}
+			// Attempt to mitigate the damage caused by the "Aerofleet bug" which I don't know how to fix.
+			catch (NullReferenceException)
+			{
+				//Log.Warning("LuluOrganCloning: AddClonedOrgans: caught NullReferenceException (aka the Aerofleet bug). Returning.");
+				return;
+			}
+		}
+	}
+}
